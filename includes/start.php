d1bc039a9aa321e53c30aea94a712e448070f981
Chris Pool <chrispool@gmail.com>
2014-11-23 16:44:56 +0100

    INIT

---
+++ includes/start.php
@@ -0,0 +1,23 @@
+<?php 
+
+
+
+public function index() {
+	//eventueel database logica en data verwerken
+	include('config/database.php');
+
+	$title = 'Dit is een test';
+
+
+	//uiteindelijk view laden
+	include('views/header.php');
+	include('views/start.php');
+	include('views/footer.php');
+}
+
+
+
+//werken met functies maakt het mogelijk om later met subpaginas te werken
+index()
+
+?>
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+
+if isset($_GET['page']) {
+	include('includes/'.$_GET['page'].'.php');
+
+} else {
+	//load default page
+	include('includes/start.php');
+}
+
+?>
