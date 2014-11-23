fb6a42f8831d5b475def9c62a86598f827f78119
Chris Pool <chrispool@gmail.com>
2014-11-23 17:50:09 +0100

    Final bootstrap fix

--- views/start.php
+++ views/start.php
@@ -2,7 +2,7 @@
 
       <div class="content">
         <h1><?php echo $title; ?></h1>
-        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+        <p class="lead">Dit is het standaard Bootstrap thema. Dit zouden we kunnen gebruiken voor het frontend</p>
       </div>
 
     </div><!-- /.container -->
