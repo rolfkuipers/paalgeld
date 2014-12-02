746db0c59d9501c49759b15e292fe624f2985c47
rolfkuipers <r.m.g.kuipers@student.rug.nl>
2014-12-02 13:58:29 +0100

    Revert "Revert "small bug fixes""
    This reverts commit c48deb97d30fe9c78fb467989d44c992c045e08e.

--- views/ship.php
+++ views/ship.php
@@ -2,7 +2,7 @@
     
       <div class="content">
      <h2>Overview for <?php echo $shipname; ?></h2>
-     <a href="index.php" class="btn btn-default">Terug</a>
+     
      <hr/>
         <?php
         echo '<table class="table table-bordered table-striped">';
